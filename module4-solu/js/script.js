94c7075f2b17b1dc240c01072f877dc7940e3a62
mm4GitHub <mariam.omobolanle.lawal@gmail.com>
2022-05-30 10:02:51 +0100

    new folder

--- module4-solu/js/script.js
+++ module4-solu/js/script.js
@@ -1,26 +1,3 @@
-// (function () {
-// var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-// var helloSpeaker = "Hello";
-// function speak(name) {
-// 	console.log(helloSpeaker + " " + name);
-// }
-
-// var byeSpeaker = "Good Bye";
-// function speak(name){
-// 	console.log(byeSpeaker + " " + name);
-// }
-
-//   for (var i = 0; i < names.length; i++) {
-//  	var firstLetter = names[i].charAt(0).toLowerCase();
-//   if (firstLetter === 'j'){
-//  	byeSpeaker.speak(names[i]);
-//   } else{
-//  	helloSpeaker.speaks(names[i]);
-//   }
-// }
-
-//  })();
-
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
